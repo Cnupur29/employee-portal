db0977f7f71f78dc23958961b1c9d84e017c7c41
surbhichaudhary <nupuryashi1@gmail.com>
2022-06-22 02:09:54 +0530

    Add swagger ui

--- src/main/java/com/schaudha/employeeportal/controller/EmployeeController.java
+++ src/main/java/com/schaudha/employeeportal/controller/EmployeeController.java
@@ -2,11 +2,15 @@ package com.schaudha.employeeportal.controller;
 
 import com.schaudha.employeeportal.model.Employee;
 import com.schaudha.employeeportal.repository.EmployeeRepository;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -22,8 +26,27 @@ public class EmployeeController {
     }
 
     @GetMapping(value = "/employees/{id}")
-    public Employee getEmployeeById(@PathVariable("id") long id ){
-        return employeeRepository.findById(Long.valueOf(id)).get();
+    @ApiOperation(value = "Get employee by id",
+            notes = "Api to fetch employee by its id",
+            response = Employee.class,
+            produces = "application/json",
+            httpMethod = "GET",
+            authorizations = {
+                @Authorization(value = "oauth2scheme", scopes = {
+                        @AuthorizationScope(scope = "", description = "fetch employee by id")
+                })
+            })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 400, message = "bad request")
+    })
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long id ){
+        Optional<Employee> employee = employeeRepository.findById(Long.valueOf(id));
+        if(!employee.isPresent()) {
+            return ResponseEntity.badRequest()
+                    .body(null);
+        }
+        return ResponseEntity.ok(employee.get());
     }
 
     @PostMapping(value = "/employee")
